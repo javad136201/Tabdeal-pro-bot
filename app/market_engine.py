23fea5074e72d529532822186cfa0798b1b4d144
javad136201 <afzali8362@gmail.com>
2026-09-07 19:53:43 +0330

    Delete app directory

--- app/market_engine.py
+++
@@ -1,9 +0,0 @@
-from .data import trades_to_candles
-from .strategy import analyze
-
-class MarketEngine:
-    def analyze(self, raw_trades):
-        candles = trades_to_candles(raw_trades)
-        result = analyze(candles)
-        result["engine"] = "v1.3-demo"
-        return result
